1050ac3206a72dbe96f5a13fb2085d77f3af51f4
KimBoris <89508080+KimBoris@users.noreply.github.com>
2022-01-27 16:36:11 +0900

    Pointer_C_Fifth_ 이해 안되는 장
    이해가 안되는 파트 였음
    
    복습예정

--- Pointer_C_Fifth/Pointer_C_Fifth/main.c
+++ Pointer_C_Fifth/Pointer_C_Fifth/main.c
@@ -3,9 +3,64 @@
 #include <string.h>	
 #include <malloc.h>	
 
-int main(void)
+int main(int argc, char* argv[])
 {
-	//int aList[3] = { 0 };
+
+	//int i = 0;
+	//for (i = 0; i < argc; ++i)
+	//{
+	//	puts(argv[i]);
+	//}
+	//puts("End");
+	//return 0;
+
+	//다중포인터
+	char ch = 'A';
+	//char*에는 char형 주소를 담는다.
+	char* pData = &ch;
+	//char**에는 char*형 주소를 담는다.
+	char** ppData = &pData;
+	//char***에는 char**형 주소를 담는다
+	char*** pppData = &ppData;
+
+	printf("%c\n", ch);
+	printf("%c\n", *pData);
+	printf("%c\n", **ppData);
+	printf("%c\n", ***pppData);
+	
+
+
+	////////////////////////////다중포인터 예제////////////////////////////////////
+
+
+
+	char* astrList[3] = { "Hello", "World", "String" };
+	//astrList의 요소 방식이 char*형이므로 char**에 담는다
+	char* *ppstrList = astrList;
+	//char** 형식 변수의 주소는 char***에 담는다.
+	char** *pppstrList = &ppstrList;
+
+	//*(char**+인덱스)의 형식은 char*이다.
+	//따라서 %s로 출력하거나 puts()로 출력한다.
+	puts(ppstrList[0]);
+	puts(ppstrList[1]);
+	puts(ppstrList[2]);
+
+
+	//char***를 두 번 간접지정하면 char*이다
+	puts(*pppstrList[0]);
+	puts(*(*(pppstrList + 0) + 1));
+	
+
+
+
+
+
+
+
+
+	return 0;
+	// int aList[3] = { 0 };
 	//int* pList = aList;
 	//해석 순서
 	//1. *pList = pList는 포인터이다
@@ -38,22 +93,46 @@ int main(void)
 	// char** *				*(char ***)==char**		int** *pppnData = &ppnData;
 
 	//char* [3] 배열을 각각의 문자열로 초기화	
-	char* astrList[3] = { "Hello", "World", "String" };
+	//char* astrList[3] = { "Hello", "World", "String" };
 
-	printf("%s\n", astrList[0]);
-	printf("%s\n", astrList[1]);
-	printf("%s\n", astrList[2]);
-	
-	printf("%s\n", astrList[0]+1);
-	printf("%s\n", astrList[1]+2);
-	printf("%s\n", astrList[2]+2);
-	
+	//printf("%s\n", astrList[0]);
+	//printf("%s\n", astrList[1]);
+	//printf("%s\n", astrList[2]);
+	//
+	//printf("%s\n", astrList[0]+1);
+	//printf("%s\n", astrList[1]+2);
+	//printf("%s\n", astrList[2]+2);
+	//
 
-	//astrList[0]이 주소 이다
-	
-	printf("%c\n", astrList[0][3]);
-	printf("%c\n", astrList[1][3]);
-	printf("%c\n", astrList[2][3]);
-	//다섯번째 강의 21분 
+	////astrList[0]이 주소 이다
+	//
+	//printf("%c\n", astrList[0][3]);
+	//printf("%c\n", astrList[1][3]);
+	//printf("%c\n", astrList[2][3]);
+
+
+
+
+
+
+
+
+	//이 위치가 int변수로 지정된 것
+
+	//이게 무슨말인지 모르겠다.
+
+
+	//이 자체를 int에 대한 주소라고 해석을 하겠다
+	//그것을 간접지정해버리면 int변수로 지정되어버리는것.
+
+	int nData = 10;//0x0019FEDC 
+	//*(int*)0x0019FEDC 주어진 Stack 영역이 맞다면...
+	//이와같은 논리도 성립이 되는것이다
+	*(int*)0x0019FEDC = 10;
+	printf("%d\n", *(int*)0x0019FEDC);
+	//변수라는 것이 메모리이다 .
+
+
+
 	return 0;
 }
